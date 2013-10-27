bcb23994523b0b019bc45b41b1bb30448299443c
Pierre Vaidie <pierre.vaidie@gmail.com>
2013-10-27 02:16:38 +0100

    add notifications

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('TwitterApp')
-  .controller('MainCtrl', ['$scope', 'Tweetpersitence', function ($scope, Tweetpersitence) {
+  .controller('MainCtrl', ['$scope', 'Tweetpersitence', 'Notifications', function ($scope, Tweetpersitence, Notifications) {
 
     //My list of tweets
     $scope.myTweets = Tweetpersitence.get()
@@ -20,19 +20,41 @@ angular.module('TwitterApp')
     	};
     	$scope.myTweets[id] = tweet;
 
-    	//Empty message
-    	$scope.newMessage = '';
-
     	//Save Tweets
     	Tweetpersitence.set($scope.myTweets);
+
+    	//Add notifications
+    	Notifications.add(
+    		'Tweet added: "' + $scope.newMessage + '"', 
+    		function() {
+    			$scope.removeTweet(id);
+    		}
+    	);
+
+    	//Empty message
+    	$scope.newMessage = '';
     }
 
     $scope.removeTweet = function(tweetId) {
+    	//Get the tweet
+    	var tweet = $scope.myTweets[tweetId];
+
     	//Remove the tweet
     	delete $scope.myTweets[tweetId];
 
     	//Save Tweets
     	Tweetpersitence.set($scope.myTweets);
+
+    	//Add notifications
+    	Notifications.add(
+    		'Tweet deleted: "' + tweet.message + '"', 
+    		function() {
+    			//Add tweet
+    			$scope.myTweets[tweet.id] = tweet;
+    			//Save Tweets
+    			Tweetpersitence.set($scope.myTweets);
+    		}
+    	);
     }
-    
+
   }]);
